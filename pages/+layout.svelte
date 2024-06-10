dd4f3db57d38b7be1f6b70e741c9f9d32b7692cb
Vincent HEUSCHLING <vincent@heuschling.com>
2024-06-10 16:59:24 +0200

    Custom Layout

---
+++ pages/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout 
+    {data}
+    title="My dashboard"
+    hideTOC
+    neverShowQueries
+>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
